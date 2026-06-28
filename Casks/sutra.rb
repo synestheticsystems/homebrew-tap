825749fdd8906fca78df96529b16c54297a2f2d2
Daniel Norman <daniel@danielnorman.net>
2026-06-27 18:14:08 -0700

    Drop unnecessary verified: param from sutra cask url

--- Casks/sutra.rb
+++ Casks/sutra.rb
@@ -2,8 +2,7 @@ cask "sutra" do
   version "0.1.4"
   sha256 "f420d87a07d9775ea4fd1f4352c7cc9016a95024f159b4b8be7b51c16460c113"
 
-  url "https://github.com/synestheticsystems/sutra/releases/download/v#{version}/Sutra-#{version}.dmg",
-      verified: "github.com/synestheticsystems/sutra/"
+  url "https://github.com/synestheticsystems/sutra/releases/download/v#{version}/Sutra-#{version}.dmg"
   name "Sutra"
   desc "Dev environment status dashboard with GUI/TUI"
   homepage "https://github.com/synestheticsystems/sutra"
